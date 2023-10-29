dac10d88fd0fdf4797f86dab0fffdd13b0cd3564
Christian <62727963+Cqc183@users.noreply.github.com>
2023-10-29 15:34:46 -0300

    Create ft_fibonacci.c

---
+++ C05/ex04/ft_fibonacci.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chquinta <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/14 16:21:57 by chquinta          #+#    #+#             */
+/*   Updated: 2023/10/14 16:24:27 by chquinta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+	{
+		return (-1);
+	}
+	else if (index == 0)
+	{
+		return (0);
+	}
+	else if (index == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+	}
+}
+/*
+#include<stdio.h>
+int main()
+{
+printf("%d", ft_fibonacci(5));
+	
+}
+*/
