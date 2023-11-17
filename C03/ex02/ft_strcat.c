4d9921bc6fe9180b8248c9d7675bec82464436e8
Christian <62727963+Cqc183@users.noreply.github.com>
2023-11-17 17:15:51 -0300

    Create ft_strcat.c

---
+++ C03/ex02/ft_strcat.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chquinta <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/12 16:56:19 by chquinta          #+#    #+#             */
+/*   Updated: 2023/08/13 09:37:21 by chquinta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/*
+int main()
+{
+	char a[] = "fuuuuuu";
+	char b[] = "sao";
+	printf("%s", ft_strcat(a, b));
+}
+*/
