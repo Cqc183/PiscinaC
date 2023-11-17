b85f13921e142f5a67a7165b5f13ccb452ba869c
Christian <62727963+Cqc183@users.noreply.github.com>
2023-11-17 17:16:28 -0300

    Create t_strncat.c

---
+++ C03/t_strncat.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chquinta <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/16 09:58:50 by chquinta          #+#    #+#             */
+/*   Updated: 2023/08/18 11:33:52 by chquinta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src [j] != '\0' && j < nb)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/*
+int main()
+{
+	char src[] = "saodjaliha";
+	char dest[] = "fuuu";
+	printf("%s", ft_strcat(dest, src, 3));
+}
+*/
